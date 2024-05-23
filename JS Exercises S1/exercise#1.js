3e2228444b2c0e8c78303fe5fa17e7807e2c71aa
shariq-yousuf <shariqyousuf01@gmail.com>
2024-05-23 20:11:33 +0500

    exe 1 solved

--- JS Exercises S1/exercise#1.js
+++ JS Exercises S1/exercise#1.js
@@ -1,40 +1,12 @@
-// first way 
-const str = "level"
-const strToArr = str.split("")
-const reversedArr = strToArr.reverse()
-const reversedStr = reversedArr.join("")
+const str = "radar"
+let reversedStr = ""
+
+for (i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i]
+}
 
 if (str === reversedStr) {
     console.log(`${true}; the word "${str} => ${reversedStr}" is palindrom.`)
 } else {
     console.log(`${false}; the word "${str} => ${reversedStr}" is not palindrom.`)
-}
-
-
-
-// second way
-const str2 = "hello"
-const reversedStr2 = str2.split("").reverse().join("")
-
-if (str2 === reversedStr2) {
-    console.log(`${true}; the word "${str2} => ${reversedStr2}" is palindrom.`)
-} else {
-    console.log(`${false}; the word "${str2} => ${reversedStr2}" is not palindrom.`)
-} 
-
-
-
-
-// third way
-const str3 = "radar"
-let reversedStr3 = ""
-
-for (i = str3.length - 1; i >= 0; i--) {
-    reversedStr3 += str3[i]
-}
-
-if (str3 === reversedStr3) {
-    console.log(`${true}; the word "${str3} => ${reversedStr3}" is palindrom.`)
-} else {
-    console.log(`${false}; the word "${str3} => ${reversedStr3}" is not palindrom.`)
 } 
---
+++ JS Exercises S2 (Array & Object)/exercise#2.js
@@ -0,0 +1,12 @@
+// 2. Find the longest word in string
+// let sentence1 = "The quick brown fox jumps over the lazy dog";
+// Your program should print "jumps" as it is the longest word in the string.
+// let sentence2 = "A journey of a thousand miles begins with a single step";
+// Your program should print "thousand" as it is the longest word in the string.
+// let sentence4 = "Supercalifragilisticexpialidocious is a very long word";
+// Your program should prnt "Supercalifragilisticexpialidocious" as it is the longest
+// word in the string.
+
+let sentence1 = "The quick brown fox jumps over the lazy dog";
+
+console.log(sentence1[1])
