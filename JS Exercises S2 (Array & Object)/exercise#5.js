3cd75f0f594f7bb477672851e3a6daccd03378bd
shariq-yousuf <shariqyousuf01@gmail.com>
2024-05-24 22:58:17 +0500

    exe 5 done

--- JS Exercises S2 (Array & Object)/exercise#5.js
+++ JS Exercises S2 (Array & Object)/exercise#5.js
@@ -8,26 +8,34 @@
 // Array of Numbers: let numbers = [5, 30, 11, 3, 16, 30, 21, 9];
 // output: [30, 30]
 
-let numbers = [34, 3, 9, 23, 16];
-let largestNumber = 0;
-let twoLargestNums = [];
+let numbersArr = [
+  [3, 9, 23, 16, 34],
+  [5, 10, 11, 3, 16],
+  [5, 30, 11, 3, 16, 30, 21, 9],
+];
+let largestNums = [];
+let index = 0;
+let coll = [];
+let howManyTimes = 2
 
-for (let i = 0; i < numbers.length; i++) {
-  if (numbers[i] > largestNumber) {
-    largestNumber = numbers[i];
+for (let i = 0; i < numbersArr.length; i++) {
+  let numbers = numbersArr[i];
+
+  for (let i = 0; i < howManyTimes; i++) {
+    let lg = 0;
+
+    for (let i = 0; i < numbers.length; i++) {
+      if (numbers[i] > lg) {
+        lg = numbers[i];
+        index = i;
+      }
+    }
+    coll.unshift(lg);
+    numbers.splice(index, 1);
   }
+
+  largestNums.push(coll);
+  coll = [];
+
+  console.log(`Largest Numbers in array ${i + 1}:`, largestNums[i]);
 }
-twoLargestNums.push(largestNumber);
-
-largestNumber = 0
-for (let i = 0; i < numbers.length; i++) {
-  if (numbers[i] > largestNumber && numbers[i] !== largestNumber) {
-    largestNumber = numbers[i];
-  }
-}
-
-
-console.log("twoLargestNumber:", twoLargestNums);
-console.log("largestNumber:", largestNumber);
-
-// if (numbers[i]) <
